4ed6a8fec8f000296a78028e780eb13546d9e0f7
KainWhite <kain.svetly@gmail.com>
2019-10-21 20:55:19 +0300

    initial lab1

--- lab1/src/com/company/Main.java
+++ lab1/src/com/company/Main.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("hello, world");
+    System.out.println("hello, lab1(hate u)");
   }
 }
